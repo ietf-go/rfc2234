65cb614298f122e0843aeebabdd29cf5aa298e59
angrifel <angrifel@users.noreply.github.com>
2022-03-12 17:04:59 -0300

    Fix comments

--- rfc2234.go
+++ rfc2234.go
@@ -1,11 +1,11 @@
 package rfc2234
 
-// IsAlphaByte test for ALPHA definition as defined in https://datatracker.ietf.org/doc/html/rfc2234#section-6.1
+// IsAlphaByte tests for ALPHA definition as defined in https://datatracker.ietf.org/doc/html/rfc2234#section-6.1
 func IsAlphaByte(b byte) bool {
 	return (b >= 'A' && b <= 'Z') || (b >= 'a' && b <= 'z')
 }
 
-// IsDigitByte test for DIGIT definition as defined in https://datatracker.ietf.org/doc/html/rfc2234#section-6.1
+// IsDigitByte tests for DIGIT definition as defined in https://datatracker.ietf.org/doc/html/rfc2234#section-6.1
 func IsDigitByte(b byte) bool {
 	return b >= '0' && b <= '9'
 }
